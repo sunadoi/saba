04f66ed1dbaa56a887c0852a51dc97be1c7c42a8
sunadoi <y.sunadoi@gmail.com>
2024-12-31 15:20:22 +0900

    http client

---
+++ net/wasabi/src/http.rs
@@ -0,0 +1,90 @@
+#![no_std]
+
+extern crate alloc;
+use crate::alloc::string::ToString;
+use alloc::string::String;
+use noli::net::lookup_host;
+use noli::net::SocketAddr;
+use noli::net::TcpStream;
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
+
+pub struct HttpClient {}
+
+impl HttpClient {
+  pub fn new() -> Self {
+    Self {}
+  }
+
+  pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+    // 名前解決
+    let ips = match lookup_host(&host) {
+      Ok(ips) => ips,
+      Err(e) => {
+        return Err(Error::Network(format!(
+          "Failed to find IP addresses: {:#?}",
+          e
+        )))
+      }
+    };
+
+    if ips.len() < 1 {
+      return Err(Error::Network("Failed to find IP addresses.".to_string()));
+    };
+
+    // TCP接続
+    let socket_addr: SocketAddr = (ips[0], port).into();
+    let mut stream = match TcpStream::connect(socket_addr) {
+      Ok(stream) => stream,
+      Err(_) => {
+        return Err(Error::Network(
+          "Failed to connect to the TCP stream".to_string(),
+        ))
+      }
+    };
+
+    // HTTPリクエスト送信
+    let mut request = String::from("GET /");
+    request.push_str(&path);
+    request.push_str(" HTTP/1.1\n");
+    request.push_str("Host: ");
+    request.push_str(&host);
+    request.push_str("\n");
+    request.push_str("Accept: text/html\n");
+    request.push_str("Connection: close\n");
+    request.push_str("\n");
+
+    let _bytes_written = match stream.write(request.as_bytes()) {
+      Ok(bytes) => bytes,
+      Err(_) => {
+        return Err(Error::Network(
+          "Failed to send a request to TCP stream".to_string(),
+        ))
+      }
+    };
+
+    // HTTPレスポンス受信
+    let mut received = Vec::new();
+    loop {
+      let mut buf = [0u8; 4096];
+      let bytes_read = match stream.read(&mut buf) {
+        Ok(bytes) => bytes,
+        Err(_) => {
+          return Err(Error::Network(
+            "Failed to receive a request from TCP stream".to_string(),
+          ))
+        }
+      };
+
+      if bytes_read == 0 {
+        break;
+      }
+      received.extend_from_slice(&buf[..bytes_read]);
+    }
+
+    match core::str::from_utf8(&received) {
+      Ok(response) => HttpResponse::new(response.to_string()),
+      Err(e) => Err(Error::Network(format!("Invalid received response: {}", e))),
+    }
+  }
+}
